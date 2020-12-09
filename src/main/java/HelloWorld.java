99b2f4f71f0ec1a8a45a3761fbf5bbd876fff91f
IvanOsoskov <75523094+IvanOsoskov@users.noreply.github.com>
2020-12-09 10:36:39 +0200

    Added hello world class

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+
+    public static void main (String [] args) {
+        System.out.println ("Hello World");
+        System.out.println ();
+        System.out.println ("Привет мир!");
+        System.out.println ();
+        /**
+         * Здесь можно писать на несколько строк коментарий
+         */
+        System.out.println("Здравствуй Мир");
+
+        System.out.println ();
+
+        System.out.print("Hello");
+        System.out.print(" ");
+        System.out.print("World");
+
+        class HTMLParser { //HTML - общепринятая аббревиатура
+        }
+    }
+}
